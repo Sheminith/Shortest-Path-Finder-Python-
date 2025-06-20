cd74a278a58750c58e62d61a6ebb3903641ce2b5
sheminith <10554sheminith@gmail.com>
2025-06-20 17:39:05 +0530

    Initializing curses and implementing basic hello world

--- shortest_path_finder.py
+++ shortest_path_finder.py
@@ -14,3 +14,16 @@ maze = [
     ["#", " ", " ", " ", " ", " ", " ", " ", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "#", "#"]
 ]
+
+def main(stdscr):
+    # create a color pair
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK) #id, foreground, background
+    blue_and_black = curses.color_pair(1)
+
+    # Add hello world text
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Hello world!", blue_and_black) #row, column, text, color_pair
+    stdscr.refresh()
+    stdscr.getch()
+
+wrapper(main)
